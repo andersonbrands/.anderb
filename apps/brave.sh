8000135b9755448cc5a367e4275da1f9e57af470
Anderson Brandao <anderson.brands@gmail.com>
2022-12-24 18:36:57 -0300

    Check for brave browser already being installed

--- apps/brave.sh
+++ apps/brave.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt install -y curl
 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+if ! hash brave-browser &> /dev/null; then
+    sudo apt install -y curl
 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-sudo apt update
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt install -y brave-browser
+    sudo apt update
+
+    sudo apt install -y brave-browser
+fi
