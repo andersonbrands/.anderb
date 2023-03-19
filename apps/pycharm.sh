60069a0c4f804c92b4a3e477e502a7d5fca7ad78
Anderson Brandao <anderson.brands@gmail.com>
2023-03-18 23:25:10 -0300

    Remove downloaded resource for pycharm

--- apps/pycharm.sh
+++ apps/pycharm.sh
@@ -5,3 +5,5 @@ curl -LO https://download.jetbrains.com/python/pycharm-community-2022.3.tar.gz
 sudo tar xzf pycharm-community-*.tar.gz -C /opt/
 
 # run with: /opt/pycharm-community-*/bin/pycharm.sh
+
+rm pycharm-community-*.tar.gz
