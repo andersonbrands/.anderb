1e8a1cf92e77d45b929be05e5f3ddb94ec2ae132
Anderson Brandao <anderson.brands@gmail.com>
2023-11-06 14:56:17 -0300

    Update node.sh

--- apps/node.sh
+++ apps/node.sh
@@ -11,4 +11,7 @@ if ! hash node &> /dev/null; then
 
     NODE_MAJOR=20
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+    sudo apt-get update
+    sudo apt-get install nodejs -y
 fi
